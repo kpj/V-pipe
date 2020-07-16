4ab7d8700fdcfe5a9b3217609984f2925420fe9b
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2020-07-16 12:29:03 +0200

    Prepare V-pipe input in batches

--- prepare_vpipe_input.py
+++ prepare_vpipe_input.py
@@ -1,4 +1,5 @@
 import os
+import collections
 from pathlib import Path
 
 import pandas as pd
@@ -6,19 +7,23 @@ import pandas as pd
 from tqdm import tqdm
 
 
-def main(root_dir):
+def main(root_dir, max_batch_size=1000):
     df = pd.read_csv(root_dir / 'results/selected_samples.csv')
     accession_list = df['accession'].tolist()
 
     df_final = pd.read_csv(root_dir / 'results/final_dataframe.csv', index_col=0)
     readlen_dict = df_final.to_dict()['avg_read_length']
 
-    name_template = 'samples_{type_}'
+    name_template = 'samples_{type_}_batch{batch:02}'
     dummy_date = '19700101'
 
+    current_batch_id = collections.defaultdict(int)
+    current_batch_size = collections.defaultdict(int)
     for accession in tqdm(accession_list):
+        # gather fastq files
         available_files = list(root_dir.glob(f'data/{accession}*.fastq'))
 
+        # detect type of experiment
         if len(available_files) == 1:
             # SE
             type_ = 'SE'
@@ -31,11 +36,15 @@ def main(root_dir):
         else:
             raise RuntimeError(available_files)
 
-        name = Path(name_template.format(type_=type_))
+        # assemble target names
+        name = Path(name_template.format(
+            type_=type_,
+            batch=current_batch_id[type_]))
         target = name / accession / dummy_date / 'raw_data'
 
         target.mkdir(parents=True, exist_ok=True)
 
+        # copy/link fastq files
         for path in available_files:
             basename = path.name
 
@@ -56,8 +65,15 @@ def main(root_dir):
 
             os.link(path, dest)
 
+        # add meta-info to tsv file
         with open(f'{name}.tsv', 'a') as fd:
             fd.write(f'{accession}\t{dummy_date}\t{readlen_dict[accession]}\n')
+
+        # handle batches
+        current_batch_size[type_] += 1
+        if current_batch_size[type_] == max_batch_size:
+            current_batch_size[type_] = 0
+            current_batch_id[type_] += 1
 
 
 if __name__ == '__main__':
